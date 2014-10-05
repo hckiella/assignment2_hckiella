e5106b4f35c320e6f9df4dfebf069b43dfe8ca49
hckiella <hckiella@gmail.com>
2014-10-05 16:03:58 +0200

    div

--- assignment2-deliverable/src/main/java/no/uio/inf5750/assignment2/dao/hibernate/HibernateStudentDAO.java
+++ assignment2-deliverable/src/main/java/no/uio/inf5750/assignment2/dao/hibernate/HibernateStudentDAO.java
@@ -3,18 +3,19 @@ package no.uio.inf5750.assignment2.dao.hibernate;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import no.uio.inf5750.assignment2.dao.StudentDAO;
 import no.uio.inf5750.assignment2.model.Student;
 
 
 public class HibernateStudentDAO implements StudentDAO {
+	
+	static Logger logger = Logger.getLogger(HibernateStudentDAO.class);
 
 	@Autowired
 	private SessionFactory sessionFactory;
@@ -25,12 +26,16 @@ public class HibernateStudentDAO implements StudentDAO {
 		
 		if(savedStudent == null) {
 			student.setId(student.hashCode());			
-			sessionFactory.getCurrentSession().save(student);
+			int i = (Integer) sessionFactory.getCurrentSession().save(student);
+			System.out.println("Saving " + student.getName() + " id: " + i + "(stdent.id: " + student.getId());
 			return student.getId();
 		}
 		
-		else
-			return savedStudent.getId();		
+		else {
+			System.out.println("Returning saved value");
+			return savedStudent.getId();
+		}
+			
 	}
 
 	@Override
@@ -48,8 +53,31 @@ public class HibernateStudentDAO implements StudentDAO {
 
 	@Override
 	public Collection<Student> getAllStudents() {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = sessionFactory.openSession();
+	    Transaction tx = null;
+	    List <Student> studentus = null;
+	    //Student students;
+	    try{
+	       tx = session.beginTransaction();
+	       @SuppressWarnings("unchecked")
+	       // This is an HQL query, not an SQL query (HQL is based on SQL, but is not 100% the same)
+	       List<Student> students = session.createQuery("FROM Student ORDER by studentId ASC").list();
+	       for(Student s: students)
+	    	   System.out.println("name " + s.getName());
+	       
+	       studentus = students;
+	       
+	      tx.commit();	       
+	       
+	    }catch (HibernateException e) {
+	    	
+	       if (tx!=null) tx.rollback();
+	       logger.error("DB query failed", e);
+	       
+	    }finally {
+	       session.close();
+	    }
+	    return studentus;
 	}
 
 	@Override
